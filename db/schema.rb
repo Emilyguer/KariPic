67e25d681ac23987fd83fc9edefef5f3932ef370
Emiguer <emilyguerrero@gmail.com>
2023-09-07 17:26:47 -0300

    Feat(Scaffold comment)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_152543) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_195705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
@@ -39,5 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_152543) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
   add_foreign_key "photos", "users"
 end
